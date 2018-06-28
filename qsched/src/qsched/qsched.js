684b27d9ea4e0c8a03102b80270f9a12f1a1ba3e
Alan Marchiori <amm042@amm042>
2018-06-28 13:14:35 -0400

    initial production build

--- qsched/src/qsched/qsched.js
+++ qsched/src/qsched/qsched.js
@@ -5,8 +5,8 @@ import {Card, CardBody, CardText, CardTitle,
 
 let fileDownload = require('js-file-download')
 
-let app = 'http://localhost:5000/qsched'
-
+//let app = 'http://localhost:5000/qsched'
+let app = 'https://3w96mpfgyb.execute-api.us-east-1.amazonaws.com/dev/qsched'
 
 class Qsched extends Component {
   constructor(props){
